ad41604ecac1289d1aed9c9a71d3552d0bc636b9
Neeraj Deshpande <30022081+neerajrd@users.noreply.github.com>
2026-03-20 15:24:45 +0530

    Updated.

--- rsprog/hello_world/src/main.rs
+++ rsprog/hello_world/src/main.rs
@@ -3,9 +3,9 @@
 // This is a basic Rust program.
 // -- It uses the chrono crate as an external dependency.
 
-use chrono::Local;
+// use chrono::Local;
 
 fn main() {
     println!("Hello World");
-    println!("Current time: {}", Local::now());
+    // println!("Current time: {}", Local::now());
 }
